61e734253ed60c309fdcc3bbf2aee0da7065fd2b
Tomoaki Suzuki <tomoaki.suzuki@pixta.co.jp>
2020-10-01 21:53:14 +0900

    boardのCRUDの機能の実装

---
+++ app/models/board.rb
@@ -0,0 +1,6 @@
+class Board < ApplicationRecord
+  belongs_to :user
+
+  validates :name,  presence: true
+  validates :description,  presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_30_003008) do
+ActiveRecord::Schema.define(version: 2020_10_01_044303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_boards_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
